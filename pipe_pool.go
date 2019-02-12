562d62828d3a0cd6252bb88cb0a4a305ef4a43a8
Tevin Zhang <mail2tevin@gmail.com>
2019-02-12 18:39:31 +0800

    Add PipePool of different implementations

--- pipe_pool.go
+++ pipe_pool.go
@@ -1,31 +1,6 @@
 package tcp
 
-import "sync"
-
-type pipePool struct {
-	pool sync.Pool
-}
-
-func newPipePool() pipePool {
-	return pipePool{sync.Pool{
-		New: func() interface{} {
-			return make(chan error, 1)
-		}},
-	}
-}
-
-func (p *pipePool) getPipe() chan error {
-	return p.pool.Get().(chan error)
-}
-
-func (p *pipePool) putBackPipe(pipe chan error) {
-	p.cleanPipe(pipe)
-	p.pool.Put(pipe)
-}
-
-func (p *pipePool) cleanPipe(pipe chan error) {
-	select {
-	case <-pipe:
-	default:
-	}
+type pipePool interface {
+	getPipe() chan error
+	putBackPipe(chan error)
 }
